add63b4e406fd6ba4eacd1ccfc0552047489df40
Francisco Ramone <131681889+Ramon-azevedo@users.noreply.github.com>
2025-04-17 11:24:25 -0300

    first commit

---
+++ src/test/java/com/financas/simulador/SimuladorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financas.simulador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
